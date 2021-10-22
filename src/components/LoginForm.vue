6fb732c0597310f4f16854252b634c3253780106
Alan Benavides <bruno@coastt.org>
2021-10-22 16:10:31 -0400

    refactor: login form to component

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,47 @@
+<script setup>
+	import { ref } from "vue";
+
+	defineEmits(["onSubmit"]);
+
+	const user = ref({
+		name: "",
+		password: "",
+	});
+</script>
+
+<template>
+	<form @submit.prevent="$emit('onSubmit', user)" class="login-form">
+		<label for="name">Nombre</label>
+		<input id="name" type="text" v-model="user.name" />
+		<label for="password">Contraseña</label>
+		<input id="password" type="password" v-model="user.password" />
+		<button type="submit">Loguearse</button>
+	</form>
+</template>
+
+<style scoped>
+	.login-form {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.login-form label {
+		margin-bottom: 1rem;
+	}
+
+	.login-form input {
+		margin-bottom: 1rem;
+	}
+
+	.login-form button {
+		margin-top: 1rem;
+		border: none;
+		padding: 7px 0;
+		width: 100%;
+		background-color: rgb(62, 175, 124);
+		color: white;
+		font-weight: 700;
+	}
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,27 +1,17 @@
 <script setup>
-	import { inject, ref } from "vue";
+	import { inject } from "vue";
+	import LoginForm from "../components/LoginForm.vue";
 	const loginProvided = inject("auth_login");
 
-	const login = () => {
-		loginProvided(data.value);
+	const login = (user) => {
+		loginProvided(user);
 	};
-
-	const data = ref({
-		name: "",
-		password: "",
-	});
 </script>
 
 <template>
 	<div class="login">
 		<h1>Iniciar Sesion</h1>
-		<form @submit.prevent="login()" class="login__fields">
-			<label for="name">Nombre</label>
-			<input id="name" type="text" v-model="data.name" />
-			<label for="password">Contraseña</label>
-			<input id="password" type="password" v-model="data.password" />
-			<button type="submit">Loguearse</button>
-		</form>
+		<LoginForm @onSubmit="login" />
 	</div>
 </template>
 
@@ -30,29 +20,5 @@
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
-	}
-	.login__fields {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-	}
-
-	.login__fields label {
-		margin-bottom: 1rem;
-	}
-
-	.login__fields input {
-		margin-bottom: 1rem;
-	}
-
-	.login__fields button {
-		margin-top: 1rem;
-		border: none;
-		padding: 7px 0;
-		width: 100%;
-		background-color: rgb(62, 175, 124);
-		color: white;
-		font-weight: 700;
 	}
 </style>
